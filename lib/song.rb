89bb1542c2e81f9f876f1e43cfa778897635bb16
Plimon Howard V <plimon.howard@gmail.com>
2020-10-05 22:46:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,17 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    artists << artist
   end
 
   def self.count
     @@count
   end
+  def self.artists=()
+    @artists = []
+  end
+  def self.artists
+    @artists
+  end
+    
 end
