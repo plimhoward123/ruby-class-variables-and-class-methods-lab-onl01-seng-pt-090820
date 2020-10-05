314ce13f1abda68e118ff270ef6dbd002165d3d5
Plimon Howard V <plimon.howard@gmail.com>
2020-10-05 22:37:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,14 @@
 class Song
-  @@count = 0
+  @@album_count = 0
   attr_accessor :song,:artist,:genre
 
   def initialize(song,artist,genre)
     @song = song
     @artist = artist
     @genre = genre
+  end
+
+  def self.count
+    @@album_count
   end
 end
